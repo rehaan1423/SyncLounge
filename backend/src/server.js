d8ebcb86f001e2a66693b4aedf8e61712562bfff
rehaan1423 <mdrehaanlodhia@gmail.com>
2026-05-31 15:56:30 +0530

    Ready to Deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+
 
 const app = express();
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use("/api/auth",authRoutes);
-app.use("/api/message",messageRoutes)
+app.use("/api/message",messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+    app.get("*",(req,res) =>{
+        res.sendFile(path.join(__dirname,"frontend/dist/index.html"));
+    });
+}
 
 app.listen(PORT,() =>{
     console.log("Server is Running 1235");
