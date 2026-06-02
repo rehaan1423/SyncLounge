b3300259be573158efd24c757044283b35206abb
rehaan1423 <mdrehaanlodhia@gmail.com>
2026-06-02 21:39:40 +0530

    Set up Login and Logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,21 +2,21 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
 
-import { generateToken } from "../lib/utils.js"; 
+import { generateToken } from "../lib/utils.js";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 
 dotenv.config();
 
 export const signup = async (req, res) => {
-    const {fullName, email, password} = req.body;
+    const { fullName, email, password } = req.body;
 
     try {
         if (!fullName || !email || !password) {
-            return res.status(400).json({message : "All fields are required"});
+            return res.status(400).json({ message: "All fields are required" });
         }
 
-        if (password.length<8){
-            return res.status(400).json({message : "All fields are required"});
+        if (password.length < 8) {
+            return res.status(400).json({ message: "Password Must be atleast 8 characters" });
         }
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -24,40 +24,74 @@ export const signup = async (req, res) => {
             return res.status(400).json({ message: "Invalid email format" });
         }
 
-        const user = await User.findOne({email});
-        if(user) return res.status(400).json({message : "user with email already exists"});
+        const user = await User.findOne({ email });
+        if (user) return res.status(400).json({ message: "user with email already exists" });
 
         const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password,salt);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = new User({
-            email,fullName,password : hashedPassword
+            email, fullName, password: hashedPassword
         });
 
-        if(newUser){
+        if (newUser) {
             const savedUser = await newUser.save();
-            generateToken(savedUser._id,res);
-    
+            generateToken(savedUser._id, res);
+
             res.status(201).json({
                 id: newUser._id,
-                fullName : newUser.fullName,
-                email : newUser.email,
-                profilePic : newUser.profilePic
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
             })
 
-            try{
-                await sendWelcomeEmail(savedUser.email,savedUser.fullName,process.env.ClientUrl)
-            }catch(error){
-                console.log("Non-fatal error: Welcome email failed to send.",error);
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, process.env.ClientUrl)
+            } catch (error) {
+                console.log("Non-fatal error: Welcome email failed to send.", error);
             }
         }
-        else{
-            res.status(400).json({message:"Invalid User data"});
+        else {
+            res.status(400).json({ message: "Invalid User data" });
         }
 
     }
-    catch(error){
-        console.log("Error in Signup Controller",error);
-        res.status(400).json({message:"Internal Server Error"});
+    catch (error) {
+        console.log("Error in Signup Controller", error);
+        res.status(400).json({ message: "Internal Server Error" });
     }
 }
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: "Invalid user credentials" });
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid user credentials" });
+
+        generateToken(user._id, res);
+        res.status(201).json({
+            id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic
+        })
+
+    } catch (error) {
+        console.error("Error in login controller",error);
+        res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
+export const logout = (_,res) => {
+    res.cookie("jwt","",{maxAge:0});
+    res.status(200).json({message:"Logged Out Successfully"})
+}
+
