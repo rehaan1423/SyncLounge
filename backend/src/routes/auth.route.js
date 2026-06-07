ba8fbc192779b7021c1e1982c3818302c5e75372
rehaan1423 <mdrehaanlodhia@gmail.com>
2026-06-08 01:06:37 +0530

    Set up ChatPage and UI Components

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -10,7 +10,7 @@ router.post("/signup",signup);
 router.post("/login",login);
 router.post("/logout",logout);
 
-router.put("update-profile",protectRoute,updateProfile);
+router.put("/update-profile",protectRoute,updateProfile);
 
 router.get("/check",protectRoute,(req,res) => res.status(200).json(req.user));
 
